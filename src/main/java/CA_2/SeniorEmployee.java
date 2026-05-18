54ac7fac34a92dda6734efad22fc68bae5a9f582
GuilhermeSaldanha13 <guisaldanhatk@email.com>
2026-05-18 10:10:00 +0100

    Add Employee subclasses: Senior, Junior, Intern, Contract

---
+++ src/main/java/CA_2/SeniorEmployee.java
@@ -0,0 +1,13 @@
+package CA_2;
+
+public class SeniorEmployee extends Employee {
+    public SeniorEmployee(String firstName, String lastName, String gender, String email, double salary,
+                          String department, String managerType, String jobTitle, String company) {
+        super(firstName, lastName, gender, email, salary, department, managerType, jobTitle, company);
+    }
+
+    @Override
+    public String getEmployeeCategory() {
+        return "Senior";
+    }
+}
